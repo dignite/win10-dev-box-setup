e74817c75985fc015ef32400fe61d570151a7c00
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2021-09-28 08:19:15 +0200

    Remove not wanted Windows apps
    Install spotify with chocolatey instead

--- SetupDeveloperMachine.ps1
+++ SetupDeveloperMachine.ps1
@@ -102,6 +102,44 @@ function ConfigureEnvironment {
         }
     }
 
+        Write-Host "Remove pre-installed apps"
+    # Microsoft junk
+    Get-AppxPackage Microsoft.*3D* | Remove-AppxPackage
+    Get-AppxPackage Microsoft.*advertising* | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Bing* | Remove-AppxPackage
+    Get-AppxPackage Microsoft.CommsPhone | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
+    Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+    Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Office.Sway | Remove-AppxPackage
+    Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
+    Get-AppxPackage Microsoft.People | Remove-AppxPackage
+    Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Wallet | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Windows.Photos | Remove-AppxPackage
+    Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
+    Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+    Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+    Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage
+    Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+    Get-AppxPackage microsoft.windowscommunicationsapps | Remove-AppxPackage
+    Get-AppxPackage Microsoft.Zune* | Remove-AppxPackage
+    Get-AppxPackage Microsoft.ScreenSketch | Remove-AppxPackage
+    Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
+
+    # Misc
+    Get-AppxPackage *Autodesk* | Remove-AppxPackage
+    Get-AppxPackage *Spotify* | Remove-AppxPackage
+
+    # Junk games
+    Get-AppxPackage king.com.* | Remove-AppxPackage
+    Get-AppxPackage *disney* | Remove-AppxPackage
+    Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
+    Get-AppxPackage *Solitaire* | Remove-AppxPackage
+    Write-Host "Removed pre-installed apps"
+
     $WallpaperImagePath = "$([Environment]::GetFolderPath("MyDocuments"))/wallpaper.jpg"
     if(!((Get-ItemProperty -path "HKCU:Control Panel\Desktop" -name WallPaper).WallPaper -Like $WallpaperImagePath)) {
         (new-object net.webclient).DownloadFile("https://unsplash.com/photos/eMBdLhYY468/download?force=true", $WallpaperImagePath)
@@ -146,6 +184,7 @@ function InstallPrograms {
     choco install ssms -y
     choco install docker-desktop -y
     choco install dotnetcore-sdk -y
+    choco install spotify -y
     scoop install yarn
     scoop install sudo
     scoop install pwsh
